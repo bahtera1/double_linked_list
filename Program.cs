72957a03154e91cd719e5be007a1ff0f3151301a
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-18 19:54:46 +0700

    add method whiile n try

--- Program.cs
+++ Program.cs
@@ -176,6 +176,19 @@ namespace double_linked_list
         static void Main(string[] args)
         {
             DoubleLinkedList obj = new DoubleLinkedList();
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from to the list");
+                    Console.WriteLine("3. View all record in the asceding order of roll number");
+                    Console.WriteLine("4. View all record in  the descending order of roll number");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit\n");
+                }
+            }
 
         }
     }

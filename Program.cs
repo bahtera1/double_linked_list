346797eb67b378078e662b7923787d8b3307cbd8
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-18 20:02:14 +0700

    add case 3,4

--- Program.cs
+++ Program.cs
@@ -213,6 +213,16 @@ namespace double_linked_list
                                     Console.WriteLine("Record with roll number " + rollNo + "deleted \n");
                             }
                             break;
+                        case '3':
+                            {
+                                obj.ascending();
+                            }
+                            break;
+                        case '4':
+                            {
+                                obj.descending();
+                            }
+                            break;
                     }
                 }
             }

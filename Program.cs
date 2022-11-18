82cf2c237dd97695dd1faf002a23da15330133a2
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-18 16:50:13 +0700

    add class node

--- Program.cs
+++ Program.cs
@@ -6,6 +6,36 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
+    class Node
+    {
+        // Node Class represent the node of doubly linked list
+        //it conscist of the information part and links to
+        //its succeding and preceeding
+        //in terms of next and provious
+
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        public Node prev;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)

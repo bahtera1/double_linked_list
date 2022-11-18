9f03e3ae26fa415d7846eb79bbff3158bf76ddd1
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-18 17:30:32 +0700

    add public bool dellnode

--- Program.cs
+++ Program.cs
@@ -99,6 +99,23 @@ namespace double_linked_list
         {
             Node previous, current;
             previous =current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            //beginning of data
+            if(current.next==null)
+            {
+                previous.next = null;
+                return true;
+            }
+            //node between two nodes in he list
+            if(current==START)
+            {
+                START = START.next;
+                if(START != null)
+                    START.prev= null;
+                return true;
+            }
+            
         }
 
 
